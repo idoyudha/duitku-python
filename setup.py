2829540efed7438c5d43a3677e4416ac2df4795f
Ido Widya Yudhatama <idowidya.yudhatama@gmail.com>
2025-04-30 15:20:02 +0700

    increate the version of sdk

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name="duitku",
-    version="0.2.0",
+    version="0.2.1",
     description="Duitku Python SDK",
     long_description=long_description,
     url="https://github.com/idoyudha/duitku-python",
