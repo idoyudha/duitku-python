728bc1a539fba0bf088650ed30b71b3d5947701e
idoyudha <idowidya.yudhatama@gmail.com>
2025-04-30 15:43:11 +0700

    chore: up the version to v0.2.2

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name="duitku",
-    version="0.2.1",
+    version="0.2.2",
     description="Duitku Python SDK",
     long_description=long_description,
     url="https://github.com/idoyudha/duitku-python",
