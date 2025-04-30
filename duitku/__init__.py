4b0eb2c7884a3a1f29196d271f96775519d9043c
Ido Widya Yudhatama <idowidya.yudhatama@gmail.com>
2025-04-30 15:05:38 +0700

    feat: add transaction service

--- duitku/__init__.py
+++ duitku/__init__.py
@@ -1,9 +1,11 @@
 from .client import DuitkuClient
 from .invoice import InvoiceService
 from .payment import PaymentService
+from .transaction import TransactionService
 
 class Duitku():
     def __init__(self):
         self.client = DuitkuClient()
         self.invoice = InvoiceService(self.client)
         self.payment = PaymentService(self.client)
+        self.transaction = TransactionService(self.client)
